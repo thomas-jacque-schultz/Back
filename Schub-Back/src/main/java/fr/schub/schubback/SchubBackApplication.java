c204d2bfbfbb2ad11db007fbaa9509755a292ac8
pisel <schultz.thomas@free.fr>
2023-11-19 12:36:08 +0100

    init spring

---
+++ Schub-Back/src/main/java/fr/schub/schubback/SchubBackApplication.java
@@ -0,0 +1,13 @@
+package fr.schub.schubback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchubBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchubBackApplication.class, args);
+    }
+
+}
---
+++ Schub-Back/src/test/java/fr/schub/schubback/SchubBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.schub.schubback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchubBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
