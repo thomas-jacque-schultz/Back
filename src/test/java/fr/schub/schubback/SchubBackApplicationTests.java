e19af8b4ea08146a500d6d4e59fd1879c17c6f98
thomas-jacque-schultz <schultz.thomas@free.fr>
2026-05-04 11:35:01 +0200

    🎉 init(back): bootstrap backend and squash legacy history

---
+++ src/test/java/fr/schub/schubback/SchubBackApplicationTests.java
@@ -0,0 +1,10 @@
+package fr.schub.schubback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchubBackApplicationTests {
+
+
+}
